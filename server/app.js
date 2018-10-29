38352270e103e12a53ae6cb78deb21cafa3cfce7
Thomas Ryszkiewicz <tcr4595@rit.edu>
2018-10-29 12:35:55 -0400

    Did half of DomoMaker part D

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-//Import Libraries
+// Import Libraries
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -17,25 +17,25 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-    if (err) {
-        console.log('could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('could not connect to database');
+    throw err;
+  }
 });
 
 let redisURL = {
-    hostname:'localhost',
-    port: 6379,
+  hostname: 'localhost',
+  port: 6379,
 };
 
 let redisPASS;
 
-if(process.env.REDISCLOUD_URL) {
-    redisURL = url.parse(process.env.REDISCLOUD_URL);
-    redisPASS = redisURL.auth.split(':')[1];
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(':')[1];
 }
 
-//pull in our routes
+// pull in our routes
 const router = require('./router.js');
 
 
@@ -45,21 +45,21 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.use(session({
-    key: 'sessionid',
-    store: new RedisStore({
-        host: redisURL.hostname,
-        port: redisURL.port,
-        pass: redisPASS,
-    }),
-    secret: 'Domo Arigato',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-    },
+  key: 'sessionid',
+  store: new RedisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPASS,
+  }),
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -68,17 +68,17 @@ app.use(cookieParser());
 
 app.use(csrf());
 app.use((err, req, res, next) => {
-    if(err.code !== 'EBADCSRFTOKEN') return next(err);
-    
-    console.log('Missing CSRF token');
-    return false;
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token');
+  return false;
 });
 
 router(app);
 
 app.listen(port, (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
